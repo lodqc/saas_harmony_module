74e5523bf355a6ac363053132dc09f0da9f56e57
freedoor <807300140@qq.com>
2024-08-28 15:02:53 +0800

    首次提交

---
+++ .arkui-x/ios/myframework/myframework.h
@@ -0,0 +1,13 @@
+//
+//  myframework.h
+//  myframework
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for myframework.
+FOUNDATION_EXPORT double myframeworkVersionNumber;
+
+//! Project version string for myframework.
+FOUNDATION_EXPORT const unsigned char myframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myframework/PublicHeader.h>
